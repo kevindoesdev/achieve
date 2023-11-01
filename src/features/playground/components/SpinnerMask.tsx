29e19f7e11b184e3d51574a3cfc270a80bb53fe8
Kevin Cod <kevindoesdev@gmail.com>
2023-11-01 16:39:50 +0000

    finished SpinnerMask experiment

--- src/features/playground/components/SpinnerMask.tsx
+++ src/features/playground/components/SpinnerMask.tsx
@@ -1,18 +1,26 @@
-import { useRef, useState } from 'react';
-import { View } from 'react-native';
-import { Button, Portal, Text, TextInput, useTheme } from 'react-native-paper';
+import { useEffect, useRef, useState } from 'react';
+import { Animated, ColorValue, Easing, View, ViewStyle } from 'react-native';
+import {
+  ActivityIndicator,
+  ActivityIndicatorProps,
+  Button,
+  Portal,
+  Text,
+  TextInput,
+  useTheme,
+} from 'react-native-paper';
 
 import { Spacer } from '../Spacer';
 import { Experiment } from '../types';
 
 const SpinnerMaskExperiment = () => {
   const theme = useTheme();
-  const [time, setTime] = useState(5000 as number | string);
+  const [time, setTime] = useState('5000');
   const [maskVisible, setMaskVisible] = useState(false);
 
   const onShowMask = async () => {
     setMaskVisible(true);
-    await new Promise(r => setTimeout(r, typeof time === 'number' ? time : 0));
+    await new Promise(r => setTimeout(r, parseInt(time, 10)));
     setMaskVisible(false);
   };
 
@@ -33,7 +41,8 @@ const SpinnerMaskExperiment = () => {
           <TextInput
             label="Milliseconds"
             value={String(time)}
-            onChangeText={text => setTime(text ? parseInt(text, 10) | 0 : '')}
+            //onChangeText={text => setTime(text ? parseInt(text, 10) | 0 : '')}
+            onChangeText={text => setTime(text.replace(/[^0-9]/g, ''))}
           />
           <Spacer />
           <View
@@ -48,30 +57,145 @@ const SpinnerMaskExperiment = () => {
   );
 };
 
+const rgbaMatcher = /^rgba\(([0-9.]+), ([0-9.]+), ([0-9.]+), ([0-9.]+)\)$/;
+export interface RGBA {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+
+export type RGBAPropertyModifier = number | ((original: number) => number);
+
+export interface RGBAModifier {
+  r?: RGBAPropertyModifier;
+  g?: RGBAPropertyModifier;
+  b?: RGBAPropertyModifier;
+  a?: RGBAPropertyModifier;
+}
+
+export const modifyRGBA = (
+  original: string,
+  { r, g, b, a }: RGBAModifier,
+): string => {
+  if (!rgbaMatcher.test(original)) {
+    return original;
+  }
+
+  const applyModifier = (
+    original: string,
+    modifier: RGBAPropertyModifier | undefined,
+  ) => {
+    if (modifier == null) {
+      return original;
+    }
+
+    if (typeof modifier === 'number') {
+      return modifier;
+    }
+
+    const num = parseFloat(original);
+    return modifier(num);
+  };
+
+  return original.replace(rgbaMatcher, (_, _r, _g, _b, _a) => {
+    return [
+      'rgba(',
+      applyModifier(_r, r),
+      ', ',
+      applyModifier(_g, g),
+      ', ',
+      applyModifier(_b, b),
+      ', ',
+      applyModifier(_a, a),
+      ')',
+    ].join('');
+  });
+};
+
 export interface SpinnerMaskProps {
   visible: boolean;
+  delay?: number;
+  fadeInDuration?: number;
+  backgroundColor?: ColorValue;
+  maskStyle?: ViewStyle;
+  activityIndicator?: ActivityIndicatorProps;
 }
 
 export const SpinnerMask = ({
   visible,
+  delay = 0,
+  fadeInDuration = 500,
+  backgroundColor,
+  maskStyle = {},
+  activityIndicator = {},
   children,
 }: React.PropsWithChildren<SpinnerMaskProps>) => {
-  const [layout, setLayout] = useState({ x: 0, y: 0, width: 0, height: 0 });
+  const [visibleDelayed, setVisibleDelayed] = useState(false);
+  const [delayTimeout, setDelayTimeout] = useState(
+    null as unknown as NodeJS.Timeout,
+  );
+  const [layout, setLayout] = useState({
+    left: 0,
+    top: 0,
+    width: 0,
+    height: 0,
+  });
   const [daRef, setDaRef] = useState(null as unknown as View | null);
 
-  //const ref = useRef(null);
+  useEffect(() => {
+    clearTimeout(delayTimeout);
+    setVisibleDelayed(false);
+
+    if (visible) {
+      // eslint-disable-next-line no-new
+      new Promise(r => {
+        setDelayTimeout(setTimeout(r, delay));
+      }).then(() => {
+        fadeIn.resetAnimation();
+        setVisibleDelayed(true);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [delay, visible]);
+
+  const fadeIn = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeIn, {
+      toValue: 1,
+      duration: fadeInDuration,
+      useNativeDriver: true,
+      easing: Easing.in(Easing.cubic),
+    }).start();
+  }, [visibleDelayed, fadeIn, fadeInDuration]);
+
+  const theme = useTheme();
+
+  const useBackgroundColor =
+    backgroundColor == null
+      ? modifyRGBA(theme.colors.surfaceVariant, { a: 0.6 })
+      : backgroundColor;
+
   return (
     <>
       <Portal>
-        {visible ? (
-          <View
+        {visibleDelayed ? (
+          <Animated.View
             style={{
-              left: layout.x + 2,
-              top: layout.y + 2,
-              width: layout.width - 4,
-              height: layout.height - 4,
-              backgroundColor: 'red',
-            }}></View>
+              justifyContent: 'center',
+              alignItems: 'center',
+              ...maskStyle,
+              ...layout,
+              opacity: fadeIn,
+              backgroundColor: maskStyle.backgroundColor || useBackgroundColor,
+            }}>
+            <ActivityIndicator
+              animating={true}
+              size={layout.height * 0.4}
+              {...activityIndicator}
+            />
+          </Animated.View>
         ) : (
           <></>
         )}
@@ -80,9 +204,9 @@ export const SpinnerMask = ({
         ref={ref => {
           setDaRef(ref);
         }}
-        onLayout={event => {
+        onLayout={() => {
           daRef?.measure((_, __, width, height, x, y) => {
-            setLayout({ x, y, width, height });
+            setLayout({ left: x, top: y, width, height });
           });
         }}>
         {children}
